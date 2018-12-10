d9ad08652ea8ad0194e634ecd040df2737695028
A.D <adrianlebaron@gmail.com>
2018-12-10 15:44:44 -0700

    bug fix

--- src/components/headerWrapper.js
+++ src/components/headerWrapper.js
@@ -4,7 +4,6 @@ class HeaderWrapper extends Component {
   render(){
     return (
       <div className='header-wrapper'>
-        ...HeaderWrapper
         <h1 className='header-wrapper__title'>Welcome to HOA Manager!</h1>
         <p className='header-wrapper__subtitle'>Please Login to continue</p>
         {this.props.children}
