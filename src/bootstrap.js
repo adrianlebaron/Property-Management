a626ee2700ab441b8aa4da71f6b4033a378138c9
A.D <adrianlebaron@gmail.com>
2018-12-14 08:19:25 -0700

    finished require auth  and created history manually

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 // import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
@@ -15,21 +15,35 @@ import Layout from './components/layout';
 
 import SignIn from './components/auth/signIn';
 import SignUp from './components/auth/signUp';
+import history from './history';
 
+import requireAuth from './components/requireAuth';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        hey there
+      </div>
+    )
+  }
+}
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch >
           <Layout>
             <Route path='/' exact component={SignIn}/>
             <Route path='/signIn'component={SignIn}/>
             <Route path='/signUp' component={SignUp}/>
+
+             <Route path='/dashboard' component={requireAuth(Dashboard)}/>
           </Layout>
 
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
