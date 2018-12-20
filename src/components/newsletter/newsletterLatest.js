053b83daef8fdf2b1ccf18015c43d178665d409b
A.D <adrianlebaron@gmail.com>
2018-12-20 11:13:19 -0700

    built a button component

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-  
+
+function Button({className, callback, text, icon}) {
+  if(icon) {
+    return (
+      <a onClick={callback} className={`${className} button`}>
+        <i className={icon}></i>
+      </a>
+    )
+  } 
+}
+ 
 class NewsletterLatest extends Component {
-   render() {
-      const { title, imageUrl, body } = this.props;
-       return (
-           <div className='newsletter-latest'>
-               <h1 className='newsletter-latest__title'>{title}</h1>
-               <img className='newsletter-latest__image' src={imageUrl}/>
-               <div className='newsletter-latest__body'>
-                <p>{body}</p>
-               </div>
-           </div>
-       )
-   }
+   
+  handleEdit = () => {
+    console.log('trying to handle edit');
+  }
+
+  render() {
+    const { title, imageUrl, body } = this.props;
+    return (
+      <div className='newsletter-latest'>
+        <h1 className='newsletter-latest__title'>{title}</h1>
+        <img className='newsletter-latest__image' src={imageUrl}/>
+        <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/>
+        <div className='newsletter-latest__body'>
+        <p>{body}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 // function mapStateToProps(state) {
